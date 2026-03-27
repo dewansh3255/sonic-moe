786e6c6a0a7d55559cf86ce83ce006f225a57344
Dewansh Khandelwal <dewansh3255@gmail.com>
2026-03-28 01:11:03 +0530

    Fix W2 TMA partition: replace grid-level w2_cta_layout with cluster layout for multicast

--- sonicmoe/functional/grouped_gemm.py
+++ sonicmoe/functional/grouped_gemm.py
@@ -2698,15 +2698,16 @@ class HopperWgmma_MoE_kernel:
                     
                     tRS_sY2 = tiled_copy_Y_r2s.get_slice(tidx).partition_D(sY2)
 
-                    y2_cta_layout = cute.make_layout((w2_n_tile_cnt, 1, mW2.shape[2]))
-                    w2_cta_layout = cute.make_layout((w2_n_tile_cnt, 1, mW2.shape[2]))
+                    # w2_cta_layout must be the cluster layout for TMA multicast (same as W1)
+                    # W2 is multicast across the M-cluster dimension.
+                    w2_cta_layout = cute.make_layout(cute.slice_(cta_layout_mnk, (None, 0, 0)).shape)
                     w2_tile_shape = (self.tile_M, self.tile_N2, self.tile_K2)
 
                     # === O3: Pre-load first W2 tile before the compute loop ===
                     w2_coord_0 = (0, 0, 0, expert_idx)
                     gW2_nk_0 = cute.local_tile(tma_tensor_w2, w2_tile_shape, w2_coord_0, proj=(None, 1, 1))
                     tWsW_0, tWgW_nkl_0 = cpasync.tma_partition(
-                        tma_atom_w2, 0, w2_cta_layout, cute.group_modes(sW2, 0, 2), cute.group_modes(gW2_nk_0, 0, 2)
+                        tma_atom_w2, cluster_coord_mnk[0], w2_cta_layout, cute.group_modes(sW2, 0, 2), cute.group_modes(gW2_nk_0, 0, 2)
                     )
                     if is_tma_warp:
                         w2_pipeline.producer_acquire(w2_producer_state)
@@ -2721,7 +2722,7 @@ class HopperWgmma_MoE_kernel:
                             w2_coord_next = (0, next_w2_n_idx, 0, expert_idx)
                             gW2_nk_next = cute.local_tile(tma_tensor_w2, w2_tile_shape, w2_coord_next, proj=(None, 1, 1))
                             tWsW_next, tWgW_nkl_next = cpasync.tma_partition(
-                                tma_atom_w2, next_w2_n_idx, w2_cta_layout, cute.group_modes(sW2, 0, 2), cute.group_modes(gW2_nk_next, 0, 2)
+                                tma_atom_w2, cluster_coord_mnk[0], w2_cta_layout, cute.group_modes(sW2, 0, 2), cute.group_modes(gW2_nk_next, 0, 2)
                             )
                             if is_tma_warp:
                                 w2_pipeline.producer_acquire(w2_producer_state)
